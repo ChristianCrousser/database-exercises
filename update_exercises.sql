44916608bbb0eb4c6145eb1c667185965d8ef71a
ChristianCrousser <christian.crousserkaiman@gmail.com>
2019-12-18 11:00:58 -0600

    second commit

---
+++ update_exercises.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+SELECT 'All albums';
+SELECT * FROM albums;
+UPDATE albums SET sales = sales * 10;
+SELECT 'Albums released before 1980';
+SELECT * FROM albums WHERE release_year < 1980;
+UPDATE albums SET release_year = REPLACE(release_year, '19', '18') WHERE release_year < 1980;
+SELECT 'Michael Jackson Albums';
+SELECT * FROM albums WHERE artist = 'Michael Jackson';
+UPDATE albums SET artist = REPLACE(artist, 'Michael Jackson', 'Michael Peter Jackson') WHERE artist = 'Michael Jackson';
